85dcda278edb11493b533fcc59227c1420858795
Eddie-Soto <esoto@nikkenlatam.com>
2022-07-20 19:04:03 -0500

    Update layoutpan.blade.php

--- resources/views/NewSignupRegional/layoutpan.blade.php
+++ resources/views/NewSignupRegional/layoutpan.blade.php
@@ -190,7 +190,7 @@
     <script src="{{ asset('regchileasset/file-upload/file-upload-with-preview.js') }}"></script>
 
     <script language="JavaScript" type="text/javascript" src="{{ asset('regchileasset/js/singup/Panama/signuppan.js?v=1.0') }} "></script>
-<script language="JavaScript" type="text/javascript" src="{{asset('regchileasset/js/singup/Ecuador/validationspan.js?v=1.0')}}"></script>
+<!--script language="JavaScript" type="text/javascript" src="{{asset('regchileasset/js/singup/Ecuador/validationspan.js?v=1.0')}}"></script-->
 <!--script src="http://signup.nikkenlatam.com/regchileasset/plugins/input-mask/jquery.inputmask.bundle.min.js"></script>
     <script src="http://signup.nikkenlatam.com/regchileasset/plugins/input-mask/input-mask.js"></script-->
 <script type="text/javascript">
