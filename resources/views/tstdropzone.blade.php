977296413ef80ce9555f9fb6535c13e7c4f49121
Eddie-Soto <esoto@nikkenlatam.com>
2022-08-10 11:55:13 -0500

    Update tstdropzone.blade.php

--- resources/views/tstdropzone.blade.php
+++ resources/views/tstdropzone.blade.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <title>PHP Laravel 6 - multiple file uploading dropzone js example</title>
-    <script src="plugin/jquery.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="plugin/bootstrap-3.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 </head>
 <body>
 
@@ -13,12 +13,12 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1><form action="" class="dropzone" method="post" enctype="multipart/form-data">
-  <input type="file" name="file" />
-</form>
-            <div>
-                <h3>Simple Upload Multiple Image By Click On Drop Box</h3>
-            </div>
+            <h1><form action="/file-upload"
+      class="dropzone"
+      id="my-awesome-dropzone">
+          
+          <input type="file" name="file" />
+      </form>
             
         </div>
     </div>
