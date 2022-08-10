5876b58e90696487fe3b7e2b6b24c5b160958d5f
Eddie-Soto <esoto@nikkenlatam.com>
2022-08-10 17:57:33 -0500

    Update tstdropzone.blade.php

--- resources/views/tstdropzone.blade.php
+++ resources/views/tstdropzone.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>PHP Laravel 6 - multiple file uploading dropzone js example</title>
     <!--script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script-->
-    <link rel="stylesheet" href="plugin/bootstrap-3.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
 <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
