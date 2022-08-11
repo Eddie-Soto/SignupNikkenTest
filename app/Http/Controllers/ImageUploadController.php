4330e797838eb49a20c4ed2aacd32bd6dc5ace77
Eddie-Soto <esoto@nikkenlatam.com>
2022-08-11 13:46:31 -0500

    Update ImageUploadController.php

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -19,26 +19,35 @@ class ImageUploadController extends Controller
 
     public function store(Request $request)
     {
+        $completecode=1234;
         $image = $request->file('file');
+
+        $nombre = $image->getClientOriginalName();
+
+       //indicamos que queremos guardar un nuevo archivo en el disco local
+       \Storage::disk('local')->put('/Images/'.$nombre,  \File::get($image));
+        $full_pathone = \Storage::disk('local')->url('Images/' .$nombre);
+
+        echo $imageName;
+        exit;
 
        //$imageName = $request->file('fileone')->getClientOriginalName();
 
-        $imageName = $image->getClientOriginalName();
-        echo $imageName;
-        exit;
+       /*$imageName = $image->getClientOriginalName();
+        
         $image->move(public_path('images'), $imageName);
 
         $imageUpload = new ImageUpload();
         $imageUpload->filename = $imageName;
         $imageUpload->save();
-        return response()->json(['success' => $imageName]);
+        return response()->json(['success' => $imageName]);*/
     }
 
     public function delete(Request $request)
     {
         $filename = $request->get('filename');
-        ImageUpload::where('filename', $filename)->delete();
-        $path = public_path() . '/images/' . $filename;
+       // ImageUpload::where('filename', $filename)->delete();
+        $path = \Storage::disk('local')->url('Images/' .$nombre);
         if (file_exists($path)) {
             unlink($path);
         }
