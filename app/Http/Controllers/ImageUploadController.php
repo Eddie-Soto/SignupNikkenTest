72221b52ef4af14012b523cec0546cc420fb9538
Eddie-Soto <esoto@nikkenlatam.com>
2022-08-10 20:35:07 -0500

    Update ImageUploadController.php

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -12,74 +12,31 @@ class ImageUploadController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function upload()
     {
-        //
+        return view('tstdropzone');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $image = $request->file('file');
+        $imageName = $image->getClientOriginalName();
+        $image->move(public_path('images'), $imageName);
+
+        $imageUpload = new ImageUpload();
+        $imageUpload->filename = $imageName;
+        $imageUpload->save();
+        return response()->json(['success' => $imageName]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\ImageUpload  $imageUpload
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ImageUpload $imageUpload)
+    public function delete(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\ImageUpload  $imageUpload
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(ImageUpload $imageUpload)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\ImageUpload  $imageUpload
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, ImageUpload $imageUpload)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\ImageUpload  $imageUpload
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(ImageUpload $imageUpload)
-    {
-        //
+        $filename = $request->get('filename');
+        ImageUpload::where('filename', $filename)->delete();
+        $path = public_path() . '/images/' . $filename;
+        if (file_exists($path)) {
+            unlink($path);
+        }
+        return $filename;
     }
 }
